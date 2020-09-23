21dc5b2d7fa2089b7f6bc10bbd3e461607d114e4
fanis-khafizov <khafizovafanis@yandex.ru>
2020-09-23 21:47:17 +0300

    multiplication added

--- main.cpp
+++ main.cpp
@@ -6,12 +6,18 @@ class longNum{
 private:
     vector<int> num;
     bool sign;
+
+    longNum left_shift(longNum number, int x) {
+        number.num.insert(number.num.begin(), x, 0);
+        //for (int i = number.num.size() - 1; i >= 0; i--) cout << number.num[i];
+        return number;
+    }
 public:
-    longNum() : sign(1) {};
+    longNum() : sign(true) {};
 
     longNum(vector<int>& n, bool sig = true) : num(n), sign(sig) {}
 
-    longNum(int n) {
+    explicit longNum(int n) {
         sign = n >= 0;
         if (n == 0) num.push_back(0);
         n = abs(n);
@@ -21,8 +27,8 @@ public:
         }
     }
 
-    longNum(string& s) {
-        sign = (s[0] == '-' ? false : true);
+    explicit longNum(string& s) {
+        sign = s[0] != '-';
         for (int i = s.length() - 1; i >= !sign; i--) {
             num.push_back(s[i] - '0');
         }
@@ -41,8 +47,8 @@ public:
         longNum second = number;
         first.sign = true;
         second.sign = true;
-        if (sign == false){
-            if (number.sign == false) {
+        if (!sign){
+            if (!number.sign) {
                 longNum res = first + second;
                 res.sign = false;
                 return res;
@@ -51,20 +57,21 @@ public:
                 return res;
             }
         } else {
-            if (number.sign == false) {
+            if (!number.sign) {
                 longNum res = first - second;
                 return res;
             }
         }
         if (first >= second) {
             for (int i = 0; i <= second.size() - 1; i++) {
-                first.num[i] = (first.num[i] + second.num[i]) % 10;
-                if (i == first.size() - 1) {
-                    if (first.num[i] + second.num[i] >= 10) first.num.push_back(1);
-                    break;
+                if ((i == first.size() - 1) && (first.num[i] + second.num[i] >= 10)) {
+                    first.num.push_back(1);
+                } else {
+                    first.num[i + 1] += ((first.num[i] + second.num[i]) >= 10);
                 }
-                first.num[i + 1] += (first.num[i] + second.num[i] >= 10);
+                first.num[i] = (first.num[i] + second.num[i]) % 10;
             }
+
             return first;
         }
         return second + first;
@@ -75,8 +82,8 @@ public:
         longNum second = number;
         first.sign = true;
         second.sign = true;
-        if (sign == false) {
-            if (number.sign == false){
+        if (!sign) {
+            if (!number.sign){
                 longNum res = second - first;
                 return res;
             } else {
@@ -85,9 +92,9 @@ public:
                 return res;
             }
         } else {
-            if (number.sign == false) {
+            if (!number.sign) {
                 longNum res = first + second;
-                res.sign = false;
+                res.sign = true;
                 return res;
             }
         }
@@ -101,7 +108,7 @@ public:
             return first;
         }
         longNum res = second - first;
-        res.sign = 0;
+        res.sign = false;
         return res;
     }
 
@@ -111,36 +118,72 @@ public:
         return res;
     }
 
+    longNum operator*(int x) {
+        vector<int> vec(num.size(), 0);
+        for (int i = 0; i < num.size() - 1; i++) {
+            vec[i + 1] = (vec[i] + num[i] * x) / 10;
+            vec[i] = (vec[i] + num[i] * x) % 10;
+        }
+        if (num[num.size() - 1] * x + vec[vec.size() - 1] >= 10) {
+            vec.push_back((num[num.size() - 1] * x + vec[vec.size() - 1]) / 10);
+            vec[vec.size() - 2] = (vec[vec.size() - 2] + num[num.size() - 1] * x) % 10;
+
+        } else {
+            vec[vec.size() - 1] = vec[vec.size() - 1] + num[num.size() - 1] * x;
+        }
+        while (vec[vec.size() - 1] == 0 && vec.size() > 1) vec.pop_back();
+        longNum res(vec);
+        return res;
+    }
+
+    longNum operator*(longNum& number) {
+        longNum first = *this;
+        longNum second = number;
+        first.sign = true;
+        second.sign = true;
+
+        longNum ans(0);
+
+        for (int i = 0; i < first.size(); i++) {
+            ans = ans + left_shift(second * first.num[i], i);
+        }
+        ans.sign = !(sign ^ number.sign);
+        return ans;
+    }
+
     bool operator>(longNum& number) {
-        if (sign > number.sign) return 1;
-        if (sign < number.sign) return 0;
+        if (sign > number.sign) return true;
+        if (sign < number.sign) return false;
         if (num.size() > number.size()) return sign;
         if (num.size() < number.size()) return !sign;
         for (int i = num.size() - 1; i >= 0; i--) {
             if (num[i] > number.num[i]) return sign;
             if (num[i] < number.num[i]) return !sign;
         }
-        return 0;
+        return false;
     }
 
     bool operator<(longNum& number) {
-        if (sign > number.sign) return 0;
-        if (sign < number.sign) return 1;
+        if (sign > number.sign) return false;
+        if (sign < number.sign) return true;
         if (num.size() < number.size()) return sign;
         if (num.size() > number.size()) return !sign;
         for (int i = num.size() - 1; i >= 0; i--) {
             if (num[i] < number.num[i]) return sign;
             if (num[i] > number.num[i]) return !sign;
         }
-        return 0;
+        return false;
     }
 
     bool operator==(longNum& number) {
+        /*
         if (sign != number.sign) return 0;
         if (num.size() != number.size()) return 0;
         for (int i = num.size() - 1; i >= 0; i--)
             if (num[i] != number.num[i]) return 0;
         return 1;
+         */
+        return !((*this > number) || (*this < number));
     }
 
     bool operator>=(longNum& number) {
@@ -152,6 +195,7 @@ public:
     }
 
     friend ostream& operator<<(ostream &out, const longNum& number);
+    friend longNum operator+=(longNum& number1, const longNum& number2);
 };
 
 ostream& operator<<(ostream &out, const longNum& number) {
@@ -160,12 +204,18 @@ ostream& operator<<(ostream &out, const longNum& number) {
     return out;
 }
 
+longNum operator+=(longNum& number1, const longNum& number2) {
+    number1 = number1 + number2;
+    return number1;
+}
+
 int main() {
     string s1, s2;
     cin >> s1 >> s2;
-
     longNum n1(s1);
     longNum n2(s2);
-    cout << (n1 + n2);
+    cout << n1 + n2 << endl;
+    cout << n1 - n2 << endl;
+    cout << n1 * n2 << endl;
     return 0;
 }
